fb204f5933b468423a19cb5a3d822ddcf6d0418c
Darius Rueckert <darius.rueckert@fau.de>
2019-05-15 13:21:55 +0200

    minibow initial

--- src/ScoringObject.cpp
+++
@@ -1,8 +0,0 @@
-/**
- * File: ScoringObject.cpp
- * Date: November 2011
- * Author: Dorian Galvez-Lopez
- * Description: functions to compute bow scores
- * License: see the LICENSE.txt file
- *
- */
